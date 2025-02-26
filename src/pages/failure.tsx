2ffb43986787c634b62e2ae71867d0eac44e13c1
Francogarcia02 <francoalbertogarcia2017@gmail.com>
2025-02-26 01:56:21 -0300

    test fix

---
+++ src/pages/failure.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+
+const Failure = () => {
+    return(
+        <div>
+            <NavBar/>
+            <div className="">
+
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Failure
