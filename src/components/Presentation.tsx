a9ae6d34093535b25427512741086df5729df6a8
Francogarcia02 <francoalbertogarcia2017@gmail.com>
2025-02-26 21:59:55 -0300

    Presentation update

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -2,10 +2,13 @@ const Presentation = () => {
     return (
       <div className="bg-image h-[450px] lg:h-[550px] drop-shadow-xl p-5 w-full flex flex-col items-between justify-between gap-4 text-center">
         <div className="flex flex-col items-start justify-center gap-2">
-            <h1 className="text-3xl bg-on lg:text-5xl font-bold uppercase p-1 ps-2 pe-2">
-                El exito es la suma de pequeños esfuerzos
+            <h1 className="text-xl text-start bg-on lg:text-5xl font-bold uppercase p-1 ps-2 pe-2">
+                El exito es la suma
             </h1>
-            <h1 className="text-3xl bg-on lg:text-5xl font-bold uppercase p-1 ps-2 pe-2">
+            <h1 className="text-xl text-start bg-on lg:text-5xl font-bold uppercase p-1 ps-2 pe-2">
+                de pequeños esfuerzos
+            </h1>
+            <h1 className="text-xl text-start bg-on lg:text-5xl font-bold uppercase p-1 ps-2 pe-2">
                 Repetidos dia tras dia
             </h1>
             
